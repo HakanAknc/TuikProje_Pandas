1792a843ae43b84bfec3910b49b29c1b643679e3
Hakan Akinci <hakan062.2001@gmail.com>
2024-08-02 15:43:54 +0300

    1Araştırma-geliştirme-istatistikleri verisi csv ye başarıyla çevrildi

--- data1-output1.py
+++ data1-output1.py
@@ -1,8 +1,21 @@
 import pandas as pd
 
-file_path = "veriSeti_duzensiz_csv/data1.csv"
-df = pd.read_csv(file_path)
+# CSV dosyasını oku
+file_path = 'veriSeti_duzenli_csv/.csv'  # Dosya yolunuzu buraya ekleyin
+df = pd.read_csv(file_path, delimiter=';')  # ";" ile ayrılmış verileri oku
 
-df = df.fillna(1)
+# Veriyi "," ile ayrılmış olarak kaydet
+# output_file_path = 'data_converted.csv'  # Çıktı dosya yolunuzu buraya ekleyin
+# df.to_csv(output_file_path, sep=',', index=False)
 
-print(df.head())
+# print("Veri başarıyla dönüştürüldü ve kaydedildi.")
+
+
+# 'Unnamed: 0' ve 'Unnamed: 1' sütunlarını kaldır
+df = df.drop(columns=['Unnamed: 0', 'Unnamed: 1'])
+
+# Veriyi , ile ayırarak kaydet
+output_file_path = 'veriSeti_duzenli_csv/data1.csv'  # Çıktı dosya yolunuzu buraya ekleyin
+df.to_csv(output_file_path, sep=',', index=False)
+
+print("Veri başarıyla dönüştürüldü, NaN değerleri silindi ve kaydedildi.")
